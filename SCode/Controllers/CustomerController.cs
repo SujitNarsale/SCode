679d951f6df009e1035f89633bfe04dd35577dda
Sujit Vitthal Narsale <sujitnarsale2002@gmail.com>
2023-11-07 12:32:26 +0530

    Initial commit

---
+++ SCode/Controllers/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SCode.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace SCode.Controllers
+{
+    public class CustomerController : Controller
+    {
+        AssignmentEntities db= new AssignmentEntities();
+        public async Task<ActionResult> Index()
+        {
+            List<Customer> lst = await db.Customers.ToListAsync();
+            return View(lst);
+        }
+
+
+        public ActionResult Create()
+        {
+            Customer model = new Customer();
+            model.Created_On = DateTime.Now;
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Customer c)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Customer cs=db.Customers.Find(id);
+            return View(cs);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer cs)
+        {
+            db.Entry<Customer>(cs).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Customer cu = db.Customers.Find(id);
+            db.Customers.Remove(cu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ SCode/Domain Model/CustomerEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCode.Domain_Model
+{
+    public class CustomerEntity
+    {     
+            public int CustomerId { get; set; }
+            public string CustomerFirstName { get; set; }
+            public string CustomerLastName { get; set; }
+            public string CustomerAddress { get; set; }
+            public string CustomerMobile { get; set; }
+            public string CustomerEmail { get; set; }
+            public DateTime CustomerBirthdate { get; set; }
+            public string CustomerPassword { get; set; }
+            public DateTime CreatedOn { get; set; }
+    }
+
+}
